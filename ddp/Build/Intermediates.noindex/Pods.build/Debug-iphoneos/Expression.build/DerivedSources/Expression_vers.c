121a75fb21e9b6cee5b6f340ac350c266ef11f79
ta-takahashi <ta-takahashi@mapion.co.jp>
2019-05-08 14:10:52 +0900

    aa

---
+++ ddp/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Expression.build/DerivedSources/Expression_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ExpressionVersionString[];
+ extern const double ExpressionVersionNumber;
+
+ const unsigned char ExpressionVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Expression  PROJECT:Pods-1" "\n";
+ const double ExpressionVersionNumber __attribute__ ((used)) = (double)1.;
