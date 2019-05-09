f1084cf89507252f8178acd689aa5803e490398f
ta-takahashi <ta-takahashi@mapion.co.jp>
2019-05-09 10:59:57 +0900

    テストコードの修正

--- CalculatorAppUITests/CalculatorAppUITests.swift
+++ CalculatorAppUITests/CalculatorAppUITests.swift
@@ -14,18 +14,25 @@ class CalculatorAppUITests: XCTestCase {
 
     override func setUp() {
 
+        super.setUp()
         continueAfterFailure = false
         app.launch()
     }
 
     override func tearDown() {
+
+        super.tearDown()
     }
 
-    func testExample() {
+    func testMoveCalcButton() {
 
-        XCTContext.runActivity(named: "初期表示でTextViewの文言を確認") { (activity) in
-            waitToAppear(for: app.textViews["main_title"])
-            XCTAssertEqual(app.textViews["main_title"].value as? String, "計算機")
-        }
+        let btn = self.app.buttons["clear_btn"]
+//        let button = XCUIApplication().buttons["clear_btn"]
+        XCTAssertTrue(btn.exists)
+        XCTAssertTrue(btn.isEnabled)
+//        XCTContext.runActivity(named: "初期表示でTextViewの文言を確認") { (activity) in
+//            waitToAppear(for: app.textViews["main_title"])
+//            XCTAssertEqual(app.textViews["main_title"].value as? String, "計算機")
+//        }
     }
 }
